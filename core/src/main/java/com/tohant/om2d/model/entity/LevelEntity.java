f8a61c9c3e853f9524e34bf43cb72fe89525891a
ansvir <svirepa.anton@gmail.com>
2023-10-17 08:38:13 +0300

    - Init creation

--- core/src/main/java/com/tohant/om2d/model/entity/LevelEntity.java
+++ core/src/main/java/com/tohant/om2d/model/entity/LevelEntity.java
@@ -5,9 +5,16 @@ import com.j256.ormlite.field.ForeignCollectionField;
 import com.j256.ormlite.table.DatabaseTable;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static com.tohant.om2d.actor.constant.Constant.GRID_HEIGHT;
+import static com.tohant.om2d.actor.constant.Constant.GRID_WIDTH;
+import static com.tohant.om2d.service.ServiceUtil.*;
 
 @DatabaseTable(tableName = "LEVEL")
 public class LevelEntity implements Serializable {
@@ -70,6 +77,15 @@ public class LevelEntity implements Serializable {
 
     public void setOfficeEntity(OfficeEntity officeEntity) {
         this.officeEntity = officeEntity;
+    }
+
+    public static LevelEntity createEmpty() {
+        int level = 0;
+        List<CellEntity> cellEntities = IntStream.range(0, GRID_HEIGHT).boxed()
+                .flatMap(r -> IntStream.range(0, GRID_WIDTH).boxed()
+                        .map(c -> new CellEntity(null, r, c, null)))
+                .collect(Collectors.toList());
+        return new LevelEntity(null, level, cellEntities);
     }
 
 }
